8b8f64a713c60ec783120a2fff6d332703fa6b1b
Juan Pablo Suarez <jp.juansuarez@gmail.com>
2024-10-20 16:22:32 -0300

    Agregado Retrofit con converter Gson

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
 }
