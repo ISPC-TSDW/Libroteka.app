4c17d6802360da47f593e1e8141df2a077cc47c1
Juan Pablo Suarez <jp.juansuarez@gmail.com>
2024-10-22 21:07:10 -0300

    Modificado: Nombres de los campos de la para el registro de usuario.

--- app/src/main/java/com/example/libroteka/data/RegisterRequest.java
+++ app/src/main/java/com/example/libroteka/data/RegisterRequest.java
@@ -1,15 +1,17 @@
 package com.example.libroteka.data;
 
 public class RegisterRequest {
-    private String nombre;
-    private String apellido;
+    private String username;
+    private String first_name;
+    private String last_name;
     private String dni;
     private String password;
     private String email;
 
-    public RegisterRequest(String nombre, String apellido, String dni, String password, String email) {
-        this.nombre = nombre;
-        this.apellido = apellido;
+    public RegisterRequest(String username, String first_name, String last_name, String dni, String password, String email) {
+        this.username = username;
+        this.first_name = first_name;
+        this.last_name = last_name;
         this.dni = dni;
         this.password = password;
         this.email = email;
