683dc90f8555f39883bf3abdf292b5682015067b
Juan Pablo Suarez <jp.juansuarez@gmail.com>
2024-12-12 22:12:27 -0300

    Hotfix: typo in url tokens

--- Backend/books/urls.py
+++ Backend/books/urls.py
@@ -35,8 +35,8 @@ urlpatterns = [
     path('ratings/', RatingManageView.as_view(), name='post_rating'),
     path('ratings/<int:pk>/', ModifyRatingView.as_view(), name='modify_rating'),
     path('user/update/', UpdateUserAPI.as_view(), name='user-update'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     path('', include(router.urls)),
 ]
